9c9f9c70fac264471f3038f2283552ab98993c5d
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2023-11-27 10:37:25 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/sistemkasir/SistemKasir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemkasir;
+
+/**
+ *
+ * @author candr
+ */
+public class SistemKasir {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
